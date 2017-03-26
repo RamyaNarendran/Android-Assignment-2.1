0dd872acc1758833462eca3463ccdbed9da1803e
Ramya <nramya1996@gmail.com>
2017-03-26 17:21:15 +0530

    Android Developement Assignment 2.1
    Hello World Program with Screenshot of the emulator

---
+++ Assignment1.1/app/src/main/java/com/example/ramya/assignment11/AssignmentOneAActivity.java
@@ -0,0 +1,13 @@
+package com.example.ramya.assignment11;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AssignmentOneAActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_assignment_one_a);
+    }
+}
